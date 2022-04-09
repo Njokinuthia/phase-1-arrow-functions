2fa9e0b42fbd6ef5d562f5f7b2f43758a2c5a917
Njoki <kinuthiash@gmail.com>
2022-04-09 23:49:11 +0300

    add all functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function(){
+ return 2000/100; 
+ }
+
+
+console.log(divide());
+
+const square = num => num * num;
+console.log(square(2))
+
+const add = (num1,num2)=>num1+num2;
+console.log(add(3,4))
